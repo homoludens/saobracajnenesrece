3604369323023588571216321d9e82efeedc04f4
Marko Djordjevic <marko@droopia.net>
2021-01-11 23:10:22 +0100

    add heatmap and slider

--- saobracaj.py
+++ saobracaj.py
@@ -4,6 +4,37 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from datetime import datetime
+import altair as alt
+import pydeck as pdk
+
+#https://github.com/streamlit/demo-uber-nyc-pickups/blob/master/streamlit_app.py
+
+
+def map(data, lat, lon, zoom):
+    st.write(pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={
+            "latitude": lat,
+            "longitude": lon,
+            "zoom": zoom,
+            "pitch": 50,
+        },
+        layers=[
+            pdk.Layer(
+                #"HeatmapLayer",
+                "HexagonLayer",
+                data=data,
+                get_position=["lon", "lat"],
+                radius=100,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+        ]
+    ))
+          
+
 
 st.title('Belgrade Traffic Accidents 2018')
 
@@ -66,3 +97,33 @@ st.map(df)
 
 #d3 = st.date_input("range, no dates", [])
 #st.write(d3)
+
+# LOADING DATA
+#DATE_TIME = "date/time"
+#DATA_URL = (
+    #"http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+#)
+
+#@st.cache(persist=True)
+#def load_data(nrows):
+    #data = pd.read_csv(DATA_URL, nrows=nrows)
+    #lowercase = lambda x: str(x).lower()
+    #data.rename(lowercase, axis="columns", inplace=True)
+    #data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    #return data
+
+#data = load_data(100000)
+
+#data
+
+# CREATING FUNCTION FOR MAPS 
+
+
+  
+df2 = df[["lat","lon"]]
+
+df2
+st.write(f"Data Points: {len(df2)}")
+
+midpoint = (np.average(df2["lat"]), np.average(df2["lon"]))
+map(df2, midpoint[0], midpoint[1], 11)
